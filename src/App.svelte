5f709b2e647c2b38a30e8ee7c51c87ff3d339f66
alpaca00 <alpaca00tuha@gmail.com>
2025-03-17 08:07:43 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import RetroComponent from './RetroComponent.svelte';
+</script>
+<style>
+   :root {
+    --background-color: black;
+    }
+  main {
+    background: var(--background-color);
+  }
+</style>
+<main>
+  <RetroComponent />
+</main>
